a325742654d50956c0ca8f8d63d935083f8e4184
gram <git@orsinium.dev>
2026-01-11 12:38:52 +0100

    vertical line benchmark

--- vline/src/main.zig
+++ vline/src/main.zig
@@ -9,16 +9,16 @@ pub export fn render() void {
     }
     const c: ff.Color = @enumFromInt(@mod(frame, 16) + 1);
     for (0..3) |_| {
-        var y: i32 = 0;
-        while (y < ff.height) {
-            const p1 = ff.Point{ .x = 0, .y = y };
-            const p2 = ff.Point{ .x = ff.width, .y = y };
+        var x: i32 = 0;
+        while (x < ff.width) {
+            const p1 = ff.Point{ .x = x, .y = 0 };
+            const p2 = ff.Point{ .x = x, .y = ff.height };
             const s = ff.LineStyle{
                 .color = c,
                 .width = 1,
             };
             ff.drawLine(p1, p2, s);
-            y += 1;
+            x += 1;
         }
     }
 }
