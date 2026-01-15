558fe4e3eaa88708a6f932649f029336e4388f8f
gram <git@orsinium.dev>
2026-01-15 19:33:29 +0100

    bigger image

--- img16/src/main.zig
+++ img16/src/main.zig
@@ -3,21 +3,19 @@ const ff = @import("firefly");
 var img: ff.Image = undefined;
 
 pub export fn boot() void {
-    var buf: [21]u8 = undefined;
+    var buf: [45]u8 = undefined;
     img = ff.loadFile("img", &buf);
 }
 
 pub export fn render() void {
-    for (0..3) |_| {
-        var y: i32 = 0;
-        while (y < ff.height) {
-            var x: i32 = 0;
-            while (x < ff.width) {
-                const p = ff.Point{ .x = x, .y = y };
-                ff.drawImage(img, p);
-                x += 16;
-            }
-            y += 1;
+    var y: i32 = 0;
+    while (y < ff.height) {
+        var x: i32 = 0;
+        while (x < ff.width) {
+            const p = ff.Point{ .x = x, .y = y };
+            ff.drawImage(img, p);
+            x += 16;
         }
+        y += 4;
     }
 }
